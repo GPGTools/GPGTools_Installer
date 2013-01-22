7b168a64af91ae5f1af9ab17a184d11a280622bb
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-01-22 02:51:32 +0100

    Use current dir as default core packages dir.

--- Makefile
+++ Makefile
@@ -5,4 +5,7 @@ PRODUCT = GPGTools_Installer
 include Dependencies/GPGTools_Core/newBuildSystem/Makefile.default
 
 $(PRODUCT):
+ifeq ("$(CORE_PKG_DIR)","")
+	@./prepare-packages.sh .
+endif
 	@./prepare-packages.sh $(CORE_PKG_DIR)
--- prepare-packages.sh
+++ prepare-packages.sh
@@ -10,23 +10,34 @@ source "$(dirname "${BASH_SOURCE[0]}")/Dependencies/GPGTools_Core/newBuildSystem
 
 # The directory which contains a subdir for each package where
 # the build _core.pkg is to be found.
-if [ "$1" == "" ]; then
+BASEDIR="$1"
+if [ "$BASEDIR" == "" ]; then
 	errExit "You have to specify the location of the core packages."
 fi
+
+echo "Looking for core packages in $BASEDIR"
+
+mkdir -p "$BUILDDIR" || errExit "Failed to create $BUILDDIR. Abort!"
+
 if [ ! -d "$1" ]; then
 	errExit "Location of core packages doesn't exist: $1"
 fi
 
-BASEDIR="$1"
+ANY_CORE_PACKAGES=$(ls -l "$BASEDIR"/*_Core.pkg 1>&2 2>/dev/null; test "$?" -eq "1" && echo "0" || echo "1")
 
-mkdir -p "$BUILDDIR" || errExit "Failed to create $BUILDDIR. Abort!"
+if [ "$ANY_CORE_PACKAGES" != "1" ]; then
+	errExit "No core packages found."
+fi
 
-REQUIRED_PACKAGES=$(grep '>\./' "$PKGPROJ_PATH" | sed "s/<string>//" | sed "s/<\/string>//" | sed 's/.\///' | sed 's/\n//')
+REQUIRED_PACKAGES=$(grep '>\.\./build/' "$PKGPROJ_PATH" | sed "s/<string>//" | sed "s/<\/string>//" | sed 's/..\/build\///' | sed 's/\n//')
 CLEAN_REQUIRED_PACKAGES=""
+
+echo "Found core pacakges: ${FOUND_CORE_PACKAGES}"
+
 # I hate those whitespaces.
 echoBold "Required core packages:"
 for package in $REQUIRED_PACKAGES; do
-	CLEAN_REQUIRED_PACKAGES="$package $CLEAN_REQUIRED_PACKAGES"
+	CLEAN_REQUIRED_PACKAGES="$CLEAN_REQUIRED_PACKAGES $package"
 	echo " * $package"
 done
 
