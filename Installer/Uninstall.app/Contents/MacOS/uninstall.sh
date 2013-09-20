4d01e667795702acd0a9a0021f6f286854a2259b
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-09-20 16:30:31 +0200

    Added some more files to be removed.

--- Installer/Uninstall.app/Contents/MacOS/uninstall.sh
+++ Installer/Uninstall.app/Contents/MacOS/uninstall.sh
@@ -31,6 +31,10 @@ function rmv () {
 	rm -f "$@"
 }
 
+# Kill the gpg-agent
+killall -9 gpg-agent
+# Kill the XPC service.
+launchctl unload /Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
 
 rmv -r /Library/Services/GPGServices.service "$HOME"/Library/Services/GPGServices.service
 rmv -r /Library/Mail/Bundles/GPGMail.mailbundle "$HOME"/Library/Mail/Bundles/GPGMail.mailbundle /Network/Library/Mail/Bundles/GPGMail.mailbundle
@@ -48,13 +52,16 @@ gkaLocation=${gkaLocation:-/Applications/GPG Keychain Access.app}
 rmv -r "$gkaLocation"
 
 if pushd /Library/LaunchAgents &>/dev/null ;then
-	rmv org.gpgtools.Libmacgpg.xpc.plist org.gpgtools.gpgmail.patch-uuid-user.plist org.gpgtools.macgpg2.fix.plist org.gpgtools.macgpg2.shutdown-gpg-agent.plist org.gpgtools.macgpg2.updater.plist
+	rmv org.gpgtools.Libmacgpg.xpc.plist org.gpgtools.gpgmail.patch-uuid-user.plist org.gpgtools.macgpg2.fix.plist org.gpgtools.macgpg2.shutdown-gpg-agent.plist org.gpgtools.macgpg2.updater.plist org.gpgtools.macgpg2.gpg-agent.plist
 	popd &>/dev/null
 fi
 
+rmv /Library/LaunchDaemons/org.gpgtools.gpgmail.patch-uuid.plist
+
 rmv "$HOME/Library/Preferences/org.gpgtools."*
+rmv "$HOME/Library/Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools."*
 rmv -r "/Library/Application Support/GPGTools" "$HOME/Library/Application Support/GPGTools"
-rmv -r "/Library/Frameworks/Libmacgpg.framework" "$HOME/Library/Frameworks/Libmacgpg.framework"
+rmv -r "/Library/Frameworks/Libmacgpg.framework" "$HOME/Library/Frameworks/Libmacgpg.framework" "$HOME/Containers/com.apple.mail/Data/Library/Frameworks/Libmacgpg.framework"
 
 pkgutil --regexp --forget 'org\.gpgtools\..*'
 
